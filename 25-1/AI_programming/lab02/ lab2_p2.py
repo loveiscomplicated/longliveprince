41db06b011344b145372ffabaf4a7d71a0895f5c
loveiscomplicated <yyjsjsjs243@gmail.com>
2025-04-09 19:49:47 +0900

    [UPDATE] - 24.04.09

--- 25-1/AI_programming/lab02/ lab2_p2.py
+++ 25-1/AI_programming/lab02/ lab2_p2.py
@@ -8,18 +8,19 @@ def num_to_string(num):
     """
     if len(str(num)) < 3:
         return ' '*(3 - len(str(num))) + str(num)
-    else:
+    elif len(str(num)) == 3:
         return str(num)
     
 
 
-
+# getting the user input
 n = int(input("Up to which number to print?: "))
 
+# initializing the list
 lst = [list(), list(), list(), list(), list(), list(), list(), list(), list(), list()]
 
+# adding values to the list
 temp = 1
-
 while temp <= n:
     for inner_lst in lst:
         if temp > n:
@@ -28,7 +29,13 @@ while temp <= n:
         temp += 1
     
 # checking if the inner list is empty. if it does, then discard it.
-for i in range(len(lst)):
-    if len(lst[i]) == 0:
-        del lst[i]
-    
+new_lst = [i for i in lst if len(i) != 0]
+
+# converting integers into string by using num_to_string function
+for inner_lst in new_lst:
+    for i in range(len(inner_lst)):
+        inner_lst[i] = num_to_string(inner_lst[i])
+        
+# printing line by line
+for inner_lst in new_lst:
+    print(''.join(inner_lst))
