d9e39f9ee7eb97ce8072b244a627a18b45e65a9b
loveiscomplicated <yyjsjsjs243@gmail.com>
2025-04-09 12:41:56 +0900

    [UPDATE] - 25.04.09

--- 25-1/data_str/start.cpp
+++ 25-1/data_str/start.cpp
@@ -2,12 +2,35 @@
 
 using namespace std;
 
-int main() {
-  char* arr = new char[10]();
-  cout << "----" << endl;
-  for (int i = 0; i < 10; i++) {
-    cout << arr[i] << endl;
-    cout << '1' << endl;
+int factorial(int n) {
+
+  if (n == 0) {
+    return 1;
+  } else {
+    return n * factorial(n - 1);
   }
+}
+
+
+int GCD(int a, int b) {
+  int big;
+  int small;
+  if (a > b) {
+    big = a;
+    small = b;
+  } else {
+  big = b;
+  small = a;
+  }
+
+  if (big % small == 0) {
+    return small;
+  } else {
+    
+  }
+
+}
+int main() {
+  cout << factorial(5) << endl;
   return 0;
 }
